b753616ddf6a28607ed301cedae7f22c477bc5f0
Luke Mahar <lukiedukester@outlook.com>
2024-08-30 23:13:18 -0700

    Removed unnecessary Header.js file, also made the page not found page more friendly

--- src/NoPage.js
+++ src/NoPage.js
@@ -2,9 +2,12 @@ function Home(props) {
 
     return (
       <>
-          <div>
-            Oops! This page doesn't exist!
-          </div>
+      <div className={"absolute inset-0 flex items-center justify-center"}>
+        <div className={"bg-gray-100/80 p-5 rounded text-4xl"}>
+                Oops! This page doesn't exist!
+            </div>
+      </div>
+          
       </>
     );
   }
